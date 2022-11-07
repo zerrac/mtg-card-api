3557eaf1bf6e9bec61d110589ebbaab45f449942
ccarrez <c.carrez@ccarrez.webu.coop>
2022-11-07 22:54:31 +0100

    typo

--- src/mtgcards/api/urls.py
+++ src/mtgcards/api/urls.py
@@ -6,7 +6,7 @@ router = routers.DefaultRouter()
 router.register(r"cards", views.CardViewSet)
 
 urlpatterns =   [
-    path('', views.cHomePageView.as_view()),
+    path('', views.HomePageView.as_view()),
     path("cards/", views.CardApiView.as_view()),
     path("api/", include(router.urls)),
 ]
